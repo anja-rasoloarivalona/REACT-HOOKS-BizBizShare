df20eccc2aa4fa93a0f3b384a4893096ad752bd9
anja <rasoloanja@gmail.com>
2020-07-20 14:00:56 -0400

    Replaced app logics with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import {Route, Switch } from 'react-router-dom'
 
@@ -11,99 +11,79 @@ import Animals from './views/Animals/Animals'
 import Errors from './components/Errors/Errors'
 import Navigation from './components/Navigation/Navigation'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      form: {
-        name: '',
-        animal: ''
-      },
-      namesList: [],
-      animalsList: [],
-      errors: []
+const App = () => {
+  const [form, setForm] = useState({
+    name: '',
+    animal: ''
+  })
+  const [lists, setlist] = useState({
+    namesList: [],
+    animalsList: []
+  })
+  const [errors, setErrors] = useState([])
+
+  useEffect(() => {
+    console.log('errors', errors)
+  }, [errors])
+
+
+  const onChangeInputHandler = (id, value) => {
+     setForm({ ...form, [id]: value })
+     errors.length !== 0 && setErrors([])
+  }
+
+  const submitInputHandler = (e, id, list, value) => {
+        e.preventDefault()
+        const isInputValid = validateInput(value)
+        if(isInputValid){
+          const updatedList = [...lists[list], value]
+          setlist({ ...lists, [list]: updatedList })
+          setForm({ ...form, [id]: ''})
+        } else {
+          addErrorHandler(`The submitted ${id} is too short. At least 3 characters are required.`)
+        }
+  }
+
+  const addErrorHandler = errorMessage => {
+    setErrors([...errors, errorMessage])
+  }
+
+  const removeErrorHandler = index => {
+    console.log('removing', index)
+    const errorsData = errors.length > 1 ? errors.splice(index, 1) : []
+    console.log('new errors', errorsData)
+    setErrors(errorsData)
+  }
+
+  const validateInput = (value) => {
+      if(value.trim().length > 2){
+        return true
+      } else {
+        return false
+      }
     }
-  }
-  submitInputHandler = (e, id, list, value) => {
-    e.preventDefault()
-    // Check if input is valid
-    const isInputValid = this.validateInput(value)
 
-    if(isInputValid){
-      this.setState(prevState => ({
-        ...prevState,
-        // Add the value to the corresponding list
-        [list]: [ ...prevState[list], value ],
-        form: {
-          ...prevState.form,
-          // Reset the corresponding input value
-          [id]: ''
-        },
-      }))
-    } else {
-      const errorMessage = `The submitted ${id} is too short. At least 3 characters are required.`
-      this.addErrorHandler(errorMessage)
-    }
-  }
-
-  onChangeInputHandler = (id, value) => {
-    this.setState(prevState => ({
-      ...prevState,
-      form: {
-        ...prevState.form,
-        [id]: value
-      },
-      errors: []
-    }))
-  }
-
-  validateInput = (value) => {
-    if(value.trim().length > 2){
-      return true
-    } else {
-      return false
-    }
-  }
-
-  addErrorHandler = errorMessage => {
-    this.setState(prevState => ({
-      ...prevState,
-      errors: [...prevState.errors, errorMessage ]
-    }))
-  }
-
-  removeErrorHandler = index => {
-    this.setState(prevState => ({
-      ...prevState,
-      errors: prevState.errors.splice(index, 1)
-    }))
-  }
-
-  
-  render() {
-    const { form, namesList, animalsList, errors } = this.state
-    return (
-      <div className="container">
-        <div className="app">
-          <Navigation />
-          {errors && <Errors errors={errors} onClick={this.removeErrorHandler}/>}
-          <div className="app__view">
-            <Switch>
-              <Route exact path="/" render={() => 
-                    <Home
-                        form={form}
-                        onChange={this.onChangeInputHandler}
-                        onSubmit={this.submitInputHandler}
-                    />
-               }/>
-              <Route path="/names" render={() => <Names namesList={namesList}/>}/>
-              <Route path="/animals" render={ () => <Animals animalsList={animalsList}/>}/>
-            </Switch>
-          </div>
+  return (
+    <div className="container">
+      <div className="app">
+        <Navigation />
+        {errors.length > 0 && <Errors errors={errors} onClick={removeErrorHandler}/>}
+        <div className="app__view">
+          <Switch>
+            <Route exact path="/" render={() => 
+                  <Home
+                      form={form}
+                      onChange={onChangeInputHandler}
+                      onSubmit={submitInputHandler}
+                  />
+             }/>
+            <Route path="/names" render={() => <Names namesList={lists.namesList}/>}/>
+            <Route path="/animals" render={ () => <Animals animalsList={lists.animalsList}/>}/>
+          </Switch>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
